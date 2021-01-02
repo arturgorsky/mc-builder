7893c0db53d50dd0b33f303dbe95878030fe57d5
arturgorsky <gorski.artur1@gmail.com>
2021-01-02 14:36:57 +0100

    Fetching and displaying order list from db

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import styles from './Order.module.css';
+import React from "react";
+import styles from "./Order.module.css";
 
-const order = (props) => (
-    <div className={styles.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+    for (let ingredient in props.ingredients) {
+        ingredients.push({
+            name: ingredient,
+            amount: props.ingredients[ingredient],
+        });
+    }
+
+    const ingredientList = ingredients.map((ingr) => (
+        <span
+            style={{
+                textTransform: "capitalize",
+                display: "inline-block",
+                margin: "0 8px",
+                border: "1px solid #eee",
+            }}
+            key={ingr.name}
+        >
+            {ingr.name} ({ingr.amount})
+        </span>
+    ));
+    return (
+        <div className={styles.Order}>
+            <p>Ingredients: {ingredientList}</p>
+            <p>
+                Price: <strong>{props.price} EUR</strong>
+            </p>
+        </div>
+    );
+};
 
 export default order;
