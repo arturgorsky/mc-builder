4e60e0af3aaab671d35c1c67239fff239b9db325
arturgorsky <gorski.artur1@gmail.com>
2021-01-20 22:25:04 +0100

    Added token to local storege and enabled retrieving it on page refreshes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from './store/acitons/index';
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -6,23 +8,38 @@ import Orders from './containers/Orders/Orders';
 import Layout from "./containers/Layout/Layout";
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import { Component } from "react";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <div>
-                <Layout>
-                    <Switch>
-                        <Route path="/" exact component={BurgerBuilder} />
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/logout" component={Logout} />
-                    </Switch>
-                </Layout>
-            </div>
-        </BrowserRouter>
-    );
+class App extends Component {
+
+    componentDidMount () {
+        this.props.onTryAutoSignup();
+    }
+
+    render () {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Layout>
+                        <Switch>
+                            <Route path="/" exact component={BurgerBuilder} />
+                            <Route path="/checkout" component={Checkout} />
+                            <Route path="/orders" component={Orders} />
+                            <Route path="/auth" component={Auth} />
+                            <Route path="/logout" component={Logout} />
+                        </Switch>
+                    </Layout>
+                </div>
+            </BrowserRouter>
+        );
+    }
+    
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(App);
--- src/store/acitons/auth.js
+++ src/store/acitons/auth.js
@@ -1,6 +1,6 @@
 import * as actionTypes from "./actionTypes";
-import axios from 'axios';
-import { API_KEY } from '../../secret';
+import axios from "axios";
+import { API_KEY } from "../../secret";
 
 export const authStart = () => {
     return {
@@ -20,49 +20,85 @@ export const authFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
         error: error,
-    }
-}
+    };
+};
 
 export const logout = () => {
+    localStorage.clear("token");
+    localStorage.clear("expirationDate");
+    localStorage.clear("userId");
     return {
-        type: actionTypes.AUTH_LOGOUT
-    }
-}
+        type: actionTypes.AUTH_LOGOUT,
+    };
+};
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
+    return (dispatch) => {
         setTimeout(() => {
             dispatch(logout());
-        } , expirationTime * 1000)
-    }
-}
+        }, expirationTime * 1000);
+    };
+};
 
 export const auth = (email, password, isSignUp) => {
-    return dispatch => {
+    return (dispatch) => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true,
-        }
+        };
         let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
         if (!isSignUp) {
             url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
         }
-        axios.post(url, authData)
-        .then(response => {
-            dispatch(authSuccess(response.data.idToken, response.data.localId));
-            dispatch(checkAuthTimeout(response.data.expiresIn));
-        })
-        .catch(error => {
-            dispatch(authFail(error.response.data.error));
-        })
-    }
-}
+        axios
+            .post(url, authData)
+            .then((response) => {
+                const expirationDate = new Date(
+                    new Date().getTime() + response.data.expiresIn * 1000
+                );
+                localStorage.setItem("token", response.data.idToken);
+                localStorage.setItem("expirationDate", expirationDate);
+                localStorage.setItem("userId", response.data.localId);
+                dispatch(
+                    authSuccess(response.data.idToken, response.data.localId)
+                );
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch((error) => {
+                dispatch(authFail(error.response.data.error));
+            });
+    };
+};
 
 export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
-        path: path
-    }
-}
+        path: path,
+    };
+};
+
+export const authCheckState = () => {
+    return (dispatch) => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(
+                localStorage.getItem("expirationDate")
+            );
+            if (expirationDate < new Date()) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem("userId");
+                dispatch(authSuccess(token, userId));
+                dispatch(
+                    checkAuthTimeout(
+                        (expirationDate.getTime() - new Date().getTime()) / 1000
+                    )
+                );
+            }
+        }
+    };
+};
