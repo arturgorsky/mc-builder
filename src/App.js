9e02a99915935fe405b3e9411ae504e89a61a5b7
arturgorsky <gorski.artur1@gmail.com>
2021-01-20 23:33:44 +0100

    Added redirect when user manually inserts url path that requires to be authorized

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import * as actions from './store/acitons/index';
+import * as actions from "./store/acitons/index";
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import Orders from './containers/Orders/Orders';
+import Orders from "./containers/Orders/Orders";
 import Layout from "./containers/Layout/Layout";
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout';
+import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
 import { Component } from "react";
 
 class App extends Component {
-
-    componentDidMount () {
+    componentDidMount() {
         this.props.onTryAutoSignup();
     }
 
-    render () {
+    render() {
+        let routes = (
+            <>
+                <Route path="/" exact component={BurgerBuilder} />
+                <Route path="/auth" component={Auth} />
+                <Redirect to="/" />
+            </>
+        );
+
+        if (this.props.isAuth) {
+            routes = (
+                <>
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/logout" component={Logout} />
+                    <Redirect to="/" />
+                </>
+            );
+        }
         return (
             <BrowserRouter>
                 <div>
                     <Layout>
                         <Switch>
-                            <Route path="/" exact component={BurgerBuilder} />
-                            <Route path="/checkout" component={Checkout} />
-                            <Route path="/orders" component={Orders} />
-                            <Route path="/auth" component={Auth} />
-                            <Route path="/logout" component={Logout} />
+                            {routes}
                         </Switch>
                     </Layout>
                 </div>
             </BrowserRouter>
         );
     }
-    
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
     return {
-        onTryAutoSignup: () => dispatch(actions.authCheckState())
-    }
-}
+        onTryAutoSignup: () => dispatch(actions.authCheckState()),
+    };
+};
 
-export default connect(null, mapDispatchToProps)(App);
+const mapStateToProps = (state) => {
+    return {
+        isAuth: state.auth.token !== null,
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
